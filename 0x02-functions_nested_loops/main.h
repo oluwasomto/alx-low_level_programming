74b4eb97450f1e8180b8f20ddec0619b89985c35
oluwasomto <somtostephen21@gmail.com>
2023-08-16 01:12:50 +0000

    the second

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#include <stdio.h>;
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -8,3 +9,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int i, int j);
